76d41fc0a1ac1916332c81a01b2637230877a2ee
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2024-02-09 14:39:33 +0600

    Buzzer

---
+++ buzzer.ino
@@ -0,0 +1,341 @@
+
+void song(int buzzerPin){
+  
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 185);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(212);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(91);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 247);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 220);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(181);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 165);
+  delay(181);
+  noTone(buzzerPin);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  // call the song function with digital pin
+  song(11);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
